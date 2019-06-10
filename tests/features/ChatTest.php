cb3c57e045f898176d3ee10eba514cd61b3adc94
Khall <ibrahimkhall212@gmail.com>
2019-06-10 23:23:55 +0100

    retry fix travis CI with Bionic

--- tests/features/ChatTest.php
+++ tests/features/ChatTest.php
@@ -45,7 +45,7 @@ class ChatTest extends TestCase
     public function testGlobal()
     {
         $route = config('khall_chat.route');
-        $this->assertSame('/messagerie', $route);
+        $this->assertEquals('/messagerie', $route);
     }
 
     /**
